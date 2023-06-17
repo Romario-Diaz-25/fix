c12f027e61101bc37108369c7392dce459f3a207
Romario-Diaz-25 <rdiaz@brokeree.tech>
2023-06-17 11:15:03 -0500

    FIX API EXAMPLE

---
+++ FixAcceptor/Program.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using QuickFix;
+
+namespace FixAcceptor
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                SessionSettings settings = new SessionSettings(@"C:\Users\romar\Desktop\FixInitiator\acceptor.cfg");
+                FixServerApplication application = new FixServerApplication();
+                FileStoreFactory storeFactory = new FileStoreFactory(settings);
+                ScreenLogFactory logFactory = new ScreenLogFactory(settings);
+                MessageFactory messageFactory = new DefaultMessageFactory();
+                SocketAcceptor acceptor
+                  = new SocketAcceptor(application, storeFactory, settings, logFactory, messageFactory);
+
+                acceptor.start();
+                Console.WriteLine("press <enter> to quit");
+                Console.Read();
+                acceptor.stop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+    public class FixServerApplication : MessageCracker, QuickFix.Application
+    {
+        public void onCreate(SessionID sessionID) { }
+        public void onLogon(SessionID sessionID) { }
+        public void onLogout(SessionID sessionID) { }
+        public void toAdmin(Message message, SessionID sessionID) { }
+        public void toApp(Message message, SessionID sessionID) { }
+        public void fromAdmin(Message message, SessionID sessionID) { }
+        public void fromApp(Message message, SessionID sessionID)
+        { crack(message, sessionID); }
+
+
+        public override void onMessage(QuickFix40.NewOrderSingle order, SessionID sessionID)
+        {
+            Symbol symbol = new Symbol();
+            Side side = new Side();
+            OrdType ordType = new OrdType();
+            OrderQty orderQty = new OrderQty();
+            Price price = new Price();
+            ClOrdID clOrdID = new ClOrdID();
+
+            order.get(ordType);
+
+            if (ordType.getValue() != OrdType.LIMIT)
+                throw new IncorrectTagValue(ordType.getField());
+
+            order.get(symbol);
+            order.get(side);
+            order.get(orderQty);
+            order.get(price);
+            order.get(clOrdID);
+
+            QuickFix40.ExecutionReport executionReport = new QuickFix40.ExecutionReport
+                (genOrderID(),
+                  genExecID(),
+                  new ExecTransType(ExecTransType.NEW),
+                  new OrdStatus(OrdStatus.FILLED),
+                  symbol,
+                  side,
+                  orderQty,
+                  new LastShares(orderQty.getValue()),
+                  new LastPx(price.getValue()),
+                  new CumQty(orderQty.getValue()),
+                  new AvgPx(price.getValue()));
+
+            executionReport.set(clOrdID);
+
+            if (order.isSetAccount())
+                executionReport.set(order.getAccount());
+
+            try
+            {
+                Session.sendToTarget(executionReport, sessionID);
+            }
+            catch (SessionNotFound) { }
+        }
+
+        public override void onMessage(QuickFix41.NewOrderSingle order, SessionID sessionID)
+        {
+            Symbol symbol = new Symbol();
+            Side side = new Side();
+            OrdType ordType = new OrdType();
+            OrderQty orderQty = new OrderQty();
+            Price price = new Price();
+            ClOrdID clOrdID = new ClOrdID();
+
+            order.get(ordType);
+
+            if (ordType.getValue() != OrdType.LIMIT)
+                throw new IncorrectTagValue(ordType.getField());
+
+            order.get(symbol);
+            order.get(side);
+            order.get(orderQty);
+            order.get(price);
+            order.get(clOrdID);
+
+            QuickFix41.ExecutionReport executionReport = new QuickFix41.ExecutionReport
+                (genOrderID(),
+                  genExecID(),
+                  new ExecTransType(ExecTransType.NEW),
+                  new ExecType(ExecType.FILL),
+                  new OrdStatus(OrdStatus.FILLED),
+                  symbol,
+                  side,
+                  orderQty,
+                  new LastShares(orderQty.getValue()),
+                  new LastPx(price.getValue()),
+                  new LeavesQty(0),
+                  new CumQty(orderQty.getValue()),
+                  new AvgPx(price.getValue()));
+
+            executionReport.set(clOrdID);
+
+            if (order.isSetAccount())
+                executionReport.set(order.getAccount());
+
+            try
+            {
+                Session.sendToTarget(executionReport, sessionID);
+            }
+            catch (SessionNotFound) { }
+        }
+
+        public override void onMessage(QuickFix42.NewOrderSingle order, SessionID sessionID)
+        {
+            Console.WriteLine("Got Order from session {0}", sessionID.toString());
+            Symbol symbol = new Symbol();
+            Side side = new Side();
+            OrdType ordType = new OrdType();
+            OrderQty orderQty = new OrderQty();
+            Price price = new Price();
+            ClOrdID clOrdID = new ClOrdID();
+
+            order.get(ordType);
+
+            if (ordType.getValue() != OrdType.LIMIT)
+                throw new IncorrectTagValue(ordType.getField());
+
+            order.get(symbol);
+            order.get(side);
+            order.get(orderQty);
+            order.get(price);
+            order.get(clOrdID);
+
+            QuickFix42.ExecutionReport executionReport = new QuickFix42.ExecutionReport
+                                                    (genOrderID(),
+                                                      genExecID(),
+                                                      new ExecTransType(ExecTransType.NEW),
+                                                      new ExecType(ExecType.FILL),
+                                                      new OrdStatus(OrdStatus.FILLED),
+                                                      symbol,
+                                                      side,
+                                                      new LeavesQty(0),
+                                                      new CumQty(orderQty.getValue()),
+                                                      new AvgPx(price.getValue()));
+
+            executionReport.set(clOrdID);
+            executionReport.set(orderQty);
+            executionReport.set(new LastShares(orderQty.getValue()));
+            executionReport.set(new LastPx(price.getValue()));
+
+            if (order.isSetAccount())
+                executionReport.set(order.getAccount());
+
+            try
+            {
+                Session.sendToTarget(executionReport, sessionID);
+            }
+            catch (SessionNotFound) { }
+        }
+
+        public override void onMessage(QuickFix43.NewOrderSingle order, SessionID sessionID)
+        {
+            Symbol symbol = new Symbol();
+            Side side = new Side();
+            OrdType ordType = new OrdType();
+            OrderQty orderQty = new OrderQty();
+            Price price = new Price();
+            ClOrdID clOrdID = new ClOrdID();
+
+            order.get(ordType);
+
+            if (ordType.getValue() != OrdType.LIMIT)
+                throw new IncorrectTagValue(ordType.getField());
+
+            order.get(symbol);
+            order.get(side);
+            order.get(orderQty);
+            order.get(price);
+            order.get(clOrdID);
+
+            QuickFix43.ExecutionReport executionReport = new QuickFix43.ExecutionReport
+                                                    (genOrderID(),
+                                                      genExecID(),
+                                                      new ExecType(ExecType.FILL),
+                                                      new OrdStatus(OrdStatus.FILLED),
+                                                      side,
+                                                      new LeavesQty(0),
+                                                      new CumQty(orderQty.getValue()),
+                                                      new AvgPx(price.getValue()));
+
+            executionReport.set(clOrdID);
+            executionReport.set(symbol);
+            executionReport.set(orderQty);
+            executionReport.set(new LastQty(orderQty.getValue()));
+            executionReport.set(new LastPx(price.getValue()));
+
+            if (order.isSetAccount())
+                executionReport.set(order.getAccount());
+
+            try
+            {
+                Session.sendToTarget(executionReport, sessionID);
+            }
+            catch (SessionNotFound) { }
+        }
+
+        public override void onMessage(QuickFix44.NewOrderSingle order, SessionID sessionID)
+        {
+            Symbol symbol = new Symbol();
+            Side side = new Side();
+            OrdType ordType = new OrdType();
+            OrderQty orderQty = new OrderQty();
+            Price price = new Price();
+            ClOrdID clOrdID = new ClOrdID();
+
+            order.get(ordType);
+
+            if (ordType.getValue() != OrdType.LIMIT)
+                throw new IncorrectTagValue(ordType.getField());
+
+            order.get(symbol);
+            order.get(side);
+            order.get(orderQty);
+            order.get(price);
+            order.get(clOrdID);
+
+            QuickFix44.ExecutionReport executionReport = new QuickFix44.ExecutionReport
+              (genOrderID(),
+              genExecID(),
+              new ExecType(ExecType.FILL),
+              new OrdStatus(OrdStatus.FILLED),
+              side,
+              new LeavesQty(0),
+              new CumQty(orderQty.getValue()),
+              new AvgPx(price.getValue()));
+
+            executionReport.set(clOrdID);
+            executionReport.set(symbol);
+            executionReport.set(orderQty);
+            executionReport.set(new LastQty(orderQty.getValue()));
+            executionReport.set(new LastPx(price.getValue()));
+
+            if (order.isSetAccount())
+                executionReport.setField(order.getAccount());
+
+            try
+            {
+                Session.sendToTarget(executionReport, sessionID);
+            }
+            catch (SessionNotFound) { }
+        }
+
+        //public override void onMessage(QuickFix50.NewOrderSingle order, SessionID sessionID)
+        //{
+        //    Symbol symbol = new Symbol();
+        //    Side side = new Side();
+        //    OrdType ordType = new OrdType();
+        //    OrderQty orderQty = new OrderQty();
+        //    Price price = new Price();
+        //    ClOrdID clOrdID = new ClOrdID();
+
+        //    order.get(ordType);
+
+        //    if (ordType.getValue() != OrdType.LIMIT)
+        //        throw new IncorrectTagValue(ordType.getField());
+
+        //    order.get(symbol);
+        //    order.get(side);
+        //    order.get(orderQty);
+        //    order.get(price);
+        //    order.get(clOrdID);
+
+        //    QuickFix50.ExecutionReport executionReport = new QuickFix50.ExecutionReport
+        //     (genOrderID(),
+        //      genExecID(),
+        //      new ExecType(ExecType.FILL),
+        //      new OrdStatus(OrdStatus.FILLED),
+        //      side,
+        //      new LeavesQty(0),
+        //      new CumQty(orderQty.getValue()));
+
+        //    executionReport.set(clOrdID);
+        //    executionReport.set(symbol);
+        //    executionReport.set(orderQty);
+        //    executionReport.set(new LastQty(orderQty.getValue()));
+        //    executionReport.set(new LastPx(price.getValue()));
+        //    executionReport.set(new AvgPx(price.getValue()));
+
+        //    if (order.isSetAccount())
+        //        executionReport.setField(order.getAccount());
+
+        //    try
+        //    {
+        //        Session.sendToTarget(executionReport, sessionID);
+        //    }
+        //    catch (SessionNotFound) { }
+        //}
+
+        private OrderID genOrderID()
+        {
+            return new OrderID((++m_orderID).ToString());
+        }
+
+        private ExecID genExecID()
+        {
+            return new ExecID((++m_execID).ToString());
+        }
+
+        private int m_orderID;
+        private int m_execID;
+    }
+}
---
+++ FixInitiator/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Windows.Forms;
+using QuickFix;
+using System.Threading;
+using System.Text;
+
+
+namespace FixInitiator
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        //[STAThread]
+        static void Main()
+        {
+            ClientInitiator app = new ClientInitiator();
+            SessionSettings settings = new SessionSettings(@"C:\Users\romar\Desktop\FixInitiator\initiator.cfg");
+            QuickFix.Application application = new ClientInitiator();
+            FileStoreFactory storeFactory = new FileStoreFactory(settings);
+            ScreenLogFactory logFactory = new ScreenLogFactory(settings);
+            MessageFactory messageFactory = new DefaultMessageFactory();
+
+            SocketInitiator initiator = new SocketInitiator(application, storeFactory, settings, logFactory, messageFactory);
+            initiator.start();
+            Thread.Sleep(3000);
+            System.Collections.ArrayList list = initiator.getSessions();
+
+            SessionID sessionID = (SessionID)list[0];
+
+            QuickFix42.NewOrderSingle order = new QuickFix42.NewOrderSingle(new ClOrdID("DLF"), new HandlInst(HandlInst.MANUAL_ORDER), new Symbol("DLF"), new Side(Side.BUY), new TransactTime(DateTime.Now), new OrdType(OrdType.LIMIT));
+            order.set(new OrderQty(45));
+            order.set(new Price(25.4d));
+            Console.WriteLine("Sending Order to Server");
+            Session.sendToTarget(order, sessionID);
+
+
+            Console.ReadLine();
+
+            initiator.stop();
+        }
+    }
+
+    public class ClientInitiator : QuickFix.Application
+    {
+
+        public void onCreate(QuickFix.SessionID value)
+        {
+            //Console.WriteLine("Message OnCreate" + value.toString());
+        }
+
+        public void onLogon(QuickFix.SessionID value)
+        {
+            //Console.WriteLine("OnLogon" + value.toString());
+        }
+
+        public void onLogout(QuickFix.SessionID value)
+        {
+            // Console.WriteLine("Log out Session" + value.toString());
+        }
+
+        public void toAdmin(QuickFix.Message value, QuickFix.SessionID session)
+        {
+            //Console.WriteLine("Called Admin :" + value.ToString());
+        }
+
+        public void toApp(QuickFix.Message value, QuickFix.SessionID session)
+        {
+            //  Console.WriteLine("Called toApp :" + value.ToString());
+        }
+
+        public void fromAdmin(QuickFix.Message value, SessionID session)
+        {
+            // Console.WriteLine("Got message from Admin" + value.ToString());
+        }
+
+        public void fromApp(QuickFix.Message value, SessionID session)
+        {
+            if (value is QuickFix42.ExecutionReport)
+            {
+                QuickFix42.ExecutionReport er = (QuickFix42.ExecutionReport)value;
+                ExecType et = (ExecType)er.getExecType();
+                if (et.getValue() == ExecType.FILL)
+                {
+                    //TODO: implement code
+                }
+            }
+
+            Console.WriteLine("Got Execution Report from Server \n" + value.ToString());
+        }
+    }
+}
